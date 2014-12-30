6d8e6bb0da29d9caf01832ff383dea46661fb79c
Gattermeier <matthias.gattermeier@gmail.com>
2014-12-30 10:55:33 -0500

    Fixes

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ $(function(){
     
     // define a date when to switch behaviour 
     var switchdate=new Date();
-    switchdate.setFullYear(2014,11,30);
+    switchdate.setFullYear(2014,11,31);
     
     var today = new Date();
     if (active === true) {
@@ -30,44 +30,44 @@ $(function(){
         switch (Math.floor(Math.random()*3)) {
         case 0:
           console.log(0);
-          $('.eoy-donate-img').attr('src',"image1.jpg");
+          $('.eoy-donate-img').attr('src',"holder.js/568x250/lava");
           $('.eoy-donate').attr("href", "https://link.to/donate1")
           break;
         case 1:
           console.log(1);
-          $('.eoy-donate-img').attr('src',"image2.jpg");
+          $('.eoy-donate-img').attr('src',"holder.js/568x250/gray");
           $('.eoy-donate').attr("href", "https://link.to/donate2")
           break;
         case 2:
           console.log(2);
-          $('.eoy-donate-img').attr('src',"image3.jpg");
+          $('.eoy-donate-img').attr('src',"holder.js/568x250/industrial");
           $('.eoy-donate').attr("href", "https://https://link.to/donate3")
           break;
         default:
           console.log("default");
-          $('.eoy-donate-img').attr('src',"image-default.jpg");
+          $('.eoy-donate-img').attr('src',"holder.js/568x250/sky");
           $('.eoy-donate').attr("href", "https://link.to/donate-default")
         }
     } else {
         // after passing switchdate do this
-        $('.eoy-donate-img').attr('src',"image-last-minute.jpg");
+        $('.eoy-donate-img').attr('src',"holder.js/568x250/sky");
         $('.eoy-donate').attr("href", "https://link.to/donate-last-minute")
     }
   
     // If the cookie does not exist show modal & set a cookie with expiration time
-    if (($.cookie('YEmodal') == null) && (enddate>today)) {
+    if (($.cookie('modal') == null) && (enddate>today)) {
             $('#fundraisemodal').modal('show');
             var expiryDate = new Date();
             if (switchdate>today) {
                 // until today is switchdate set cookie expiration to number of hours
-                var hours = 6; 
+                var hours = 0; 
             } else {
                 // after passing switchdate set cookie expiration to new frequency
-                var hours = 1; // 1
+                var hours = 0; 
             }
             // set cookie 
             expiryDate.setTime(expiryDate.getTime() + (hours * 3600 * 1000));
-            $.cookie("YEmodal", "false", { path: '/', expires: expiryDate });
+            $.cookie("modal", "false", { path: '/', expires: expiryDate });
        } 
     }
     }
